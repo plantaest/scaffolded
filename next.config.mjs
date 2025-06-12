c527aa1feba9b9dfce97b348cac7e9fa02661f2f
Plantaest <plantaest@gmail.com>
2025-06-13 05:48:48 +0700

    chore: output standalone

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: 'standalone',
 };
 
 export default withBundleAnalyzer(withNextIntl(nextConfig));
